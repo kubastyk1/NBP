5a199c8684c4c8f8a4ede5845b0f158a98c2e9f4
Jakub Stawiszyński <kubastyk@wp.pl>
2017-06-10 17:41:40 +0200

    BankAPI class with basic methods added

---
+++ nbp/src/main/java/pl/parser/nbp/BankAPI.java
@@ -0,0 +1,21 @@
+package pl.parser.nbp;
+
+import java.util.Date;
+
+public class BankAPI {
+
+	public void makeRequest(Currency currency, Date startDate, Date endDate){
+
+	}
+
+	public double getMeanBuyRent(){
+
+		return 0;
+	}
+
+	public double getStandardDeviationSellRent(){
+
+
+		return 0;
+	}
+}
